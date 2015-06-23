f885193fb125642e749de1af910dd8dccf2c4285
jaewook.lee <jaewooki.lee@lge.com>
2015-06-23 10:35:47 +0900

    upload 03_1_file_first_template for VS2010

---
+++ 03_1_file_first_template/VS2010/model_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "common.h"
+
+boolean model_file_read(const char* fileName, FILE** writeFp)
+{
+	return true;
+}
+boolean model_file_write(const char* fileName, FILE* readFp)
+{
+	return true;
+}
+boolean model_file_buildStack(FILE* readFp)
+{
+	/*
+	content example
+	File
+	1
+	2
+	3
+	4
+	5
+
+	to Stack
+	1
+	2
+	3
+	4
+	5
+	*/
+
+	return true;
+}
