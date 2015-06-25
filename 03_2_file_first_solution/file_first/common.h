6bdf8b714c0911af28e3e6366e2102f77a3e19bc
jaewook.lee <jaewooki.lee@lge.com>
2015-06-25 22:04:19 +0900

    Delete unneeded files

--- 03_2_file_first_solution/file_first/common.h
+++
@@ -1,8 +0,0 @@
-#ifndef _COMMON_H_
-#define _COMMON_H_
-#define true (1)
-#define false (0)
-typedef int boolean;
-
-
-#endif // !_COMMON_H_
--- 03_2_file_first_solution/file_first/main.c
+++
@@ -1,9 +0,0 @@
-#include "model_data.h"
-#include "view_ui.h"
-
-int main()
-{
-	model_initialize();
-	view_frontEnd();
-	model_finalize();
-}
